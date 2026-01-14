442bbf0536fbd86c25b34d0fdd593a210bf35cfe
kavyangaHA <kavyangahathurusinghe1234@gmail.com>
2026-01-14 22:36:21 +0530

    add necessary action_module and memory system components

--- framework/agent.py
+++ framework/agent.py
@@ -1,4 +1,6 @@
 from .decision_engine import DecisionEngine
+from .action_module import ActionModule
+from .memory_system import MemorySystem
 
 
 class Agent:
@@ -8,15 +10,19 @@ class Agent:
         self.memory ={}
         self.environment = None #will be set when the agent is added to an environment
         #mulinm agent wwa create krpu gmn eyw environment ekakat add krn kl eyge environment ek None
+        
+        #initialize the brain,hands and memory
         self.decision_engine = DecisionEngine() # to give the agent a brain
-
+        self.action_module = ActionModule()   # to give the agent hands
+        self.memory_system = MemorySystem()   # to give the agent memory
+    
     def perceive(self):
 
         print(f"[{self.name}] is perceiving the environment.")
         if not self.environment:
             print(f"[{self.name}] has no environment to perceive.")
             return {}
-        perception = self.environment.get_state_for_agent(self.name)
+        perception = self.environment.get_state_for_agent(self.name)#environment cls ekedi agentwai 
         print(f"[{self.name}] perceived: {perception}")
         return perception
     
@@ -29,12 +35,18 @@ class Agent:
     
     def act(self,decision:str):
         print(f"[{self.name}] is acting...")
-        print(f"[{self.name}] performed action: {decision}(placeholder)")
-
+        #print(f"[{self.name}] performed action: {decision}(placeholder)")
+        result = self.action_module.execute(decision, context={"agent": self})
+        print(f"[{self.name}] Action result: {result}")
+        return result
+    
     def run_cycle(self):
         print(f"\n-- starting cycle for [{self.name}] --")
         perception = self.perceive()
         decision = self.decide(perception)
         self.act(decision)
+        action_result = self.act(decision)
+        #store the result of the cycle in memory
+        self.memory_system.store("last_action_result", action_result)
         print(f"--cycle for [{self.name}] finished --\n")
 
